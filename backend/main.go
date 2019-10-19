8a2de3b625151c3dfc304bb3be633d652722e6fb
Alec Stubbs <alec@thecabbageshop.org>
2019-10-19 12:00:27 +0100

    Added a vue site using vuecli

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -15,20 +14,23 @@ func main() {
 	log.Println("Starting home-site server")
 	const port = 8081
 
+	//frontendui := ""
+
 	ui := flag.String("ui", "", "ui location")
 	flag.Parse()
 	if *ui != "" {
-		http.Handle("/", http.FileServer(http.Dir(*ui)))
 		log.Printf("Address: http://localhost:%v UI dir is %v", port, *ui)
-		log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 	} else {
 		log.Fatalln("No -ui flag. Please supply the location of the frontend. e.g -ui=\"../frontend/dist\"")
 	}
 	r := httprouter.New()
-	r.GET("/", index)
-	http.ListenAndServe("localhost:8080", r)
+	//	r.GET("/", index)
+	r.Handler("GET", "/", http.FileServer(http.Dir(*ui)))
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), r))
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
 	fmt.Fprint(w, "Working")
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cabbageshop/home-site
+
+go 1.12
+
+require github.com/julienschmidt/httprouter v1.3.0
